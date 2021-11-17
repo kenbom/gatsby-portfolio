3878d22748cae29d9d41005e4073509b82e55dee
kenbom <tsutomu.kimura@nifty.ne.jp>
2021-11-17 07:39:51 +0000

    add MicroCMS configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,26 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+require("dotenv").config(/*{
+  path: `.env.${process.env.NODE_ENV}`,
+}*/)
 
 module.exports = {
   /* Your site config here */
 
   plugins: [
+    {
+      resolve: "gatsby-source-microcms",
+      options: {
+        apiKey: process.env.API_KEY,
+        serviceId: "nekohige",
+        apis: [
+          {
+            endpoint: "job",
+          },
+        ],
+      },
+    },
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
