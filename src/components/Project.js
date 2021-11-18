657560d43cd791d8f7882aca31fdd270096a165b
kenbom <tsutomu.kimura@nifty.ne.jp>
2021-11-18 23:02:36 +0000

    modify project component

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,8 +3,28 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { FaGithubSquare, FaShareSquare } from "react-icons/fa"
 import { Link } from "gatsby"
 
-const Project = () => {
-  return <h2>single project component</h2>
+const Project = ({
+  description,
+  title,
+  github,
+  url,
+  slug,
+  index,
+  pjid,
+  image,
+}) => {
+  return (
+    <article className="project">
+      <img src={image.url} className="project-img" alt={title}></img>
+      <div className="project-info">
+        <span className="project-number">0{index + 1}.</span>
+        <Link to={`/projects/${slug}`} className="project-slug">
+          <h3>{title}</h3>
+        </Link>
+        <p className="project-desc">{description}</p>
+      </div>
+    </article>
+  )
 }
 
 export default Project
--- src/pages/index.js
+++ src/pages/index.js
@@ -31,6 +31,7 @@ export const query = graphql`
         github
         pjid
         slug
+        title
         url
         image {
           url
