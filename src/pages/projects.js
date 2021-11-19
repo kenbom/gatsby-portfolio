cc730448167ee374822268c9f8123f64b45263e9
kenbom <tsutomu.kimura@nifty.ne.jp>
2021-11-19 05:21:46 +0000

    modify project page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,12 +2,40 @@ import React from "react"
 import { graphql } from "gatsby"
 import AllProjects from "../components/AllProjects"
 import Seo from "../components/Seo"
-const ProjectsPage = () => {
+
+const ProjectsPage = ({
+  data: {
+    allMicrocmsProject: { nodes: projects },
+  },
+}) => {
   return (
     <>
-      <h2>projects page</h2>
+      <main>
+        <section className="project-page">
+          <AllProjects title="all projects" showLink projects={projects}></AllProjects>
+        </section>
+      </main>
     </>
   )
 }
+
+export const query = graphql`
+  {
+    allMicrocmsProject {
+      nodes {
+        description
+        featured
+        github
+        pjid
+        slug
+        title
+        url
+        image {
+          url
+        }
+      }
+    }
+  }
+`
 
 export default ProjectsPage
