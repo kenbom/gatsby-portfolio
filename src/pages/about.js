19391c764ba42cf5ee3ab1afe795cd19e86ff5b7
kenbom <tsutomu.kimura@nifty.ne.jp>
2021-11-19 18:15:41 +0000

    add about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,21 @@
 import React from "react"
-import { graphql } from "gatsby"
 import Title from "../components/Title"
 import Seo from "../components/Seo"
+import heroImg from "../assets/images/hero.svg"
 
-const About = () => {
+const About = ( ) => {
   return (
     <>
-      <h2>about page</h2>
+      <Seo title="About" />
+      <section className="about-page">
+        <div className="section-center about-center">
+          <img src={heroImg} alt="about me" className="about-img-svg" />
+          <article className="about-text">
+            <Title title= "about me"/>
+            <p>test</p>
+          </article>
+        </div>
+      </section>
     </>
   )
 }
